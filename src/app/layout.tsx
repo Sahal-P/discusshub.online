00fba84dd059843b99a33c5247c5f620faedbd10
Sahal-P <sahlcyprus@gmail.com>
2023-09-27 20:43:42 +0530

    perfecting the authentication flow

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import Navbar from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import '@/styles/globals.css'
+import {Nunito} from "next/font/google"
+import { Toaster } from "@/components/ui/Toaster"
+import React from 'react'
+
+export const metadata = {
+  title: 'Discusshub',
+  description: 'A social news aggregation, content rating, and discussion website. Registered users submit content to the site such as links, text posts, images, and videos',
+}
+
+const nunito = Nunito({ subsets: ["latin"]})
+
+export default function RootLayout({
+  children,
+  authModal
+}: {
+  children: React.ReactNode
+  authModal: React.ReactNode
+}) {
+  return (
+    <html lang='en' className={cn(
+      "bg-white text-slate-900 antialiased light",
+      nunito.className
+    )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        {/* @ts-expect-error server component */}
+        <Navbar />
+        
+        {authModal}
+
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+        {children}
+        </div>
+        <Toaster />
+      </body>
+    </html>
+  )
+}
